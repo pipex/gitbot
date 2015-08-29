030a786db1c0602125bfe4093c6a5709b0202858
Felipe Lalanne <flalanne@niclabs.cl>
2015-08-29 21:41:04 +0200

    Add default hook url for gitlab

--- app/hooks/views.py
+++ app/hooks/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from app import app, webhooks
 
-@webhooks.hook(app.config.get('GITLAB_HOOK'), handler='gitlab')
+@webhooks.hook(
+    app.config.get('GITLAB_HOOK','/hooks/gitlab'),
+    handler='gitlab')
 class Gitlab:
     def issue(self, data):
         pass
