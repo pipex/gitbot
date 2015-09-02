3bf0d76f67c3474b20b1dd82d7ad7743f8aadee7
Felipe Lalanne <flalanne@niclabs.cl>
2015-09-02 17:57:56 -0300

    Create tag_push event tests

--- tests/gitlab.py
+++ tests/gitlab.py
@@ -5,6 +5,48 @@ from flask import json
 from .base import BaseTestCase
 
 class GitlabTestCase(BaseTestCase):
+    def test_tag_push_hook(self):
+        data = {
+            u'ref': u'refs/tags/0.0.1',
+            u'user_id': 3,
+            u'object_kind': u'tag_push',
+            u'repository': {
+                u'git_ssh_url': u'git@git.niclabs.cl:super-project/test-project.git',
+                u'name': u'test-project',
+                u'url': u'git@git.niclabs.cl:super-project/test-project.git',
+                u'git_http_url': u'http://git.niclabs.cl/super-project/test-project.git',
+                u'visibility_level': 0,
+                u'homepage': u'http://git.niclabs.cl/super-project/test-project',
+                u'description': u'My test project'
+            },
+            u'commits': [
+                {
+                    u'url': u'http://git.niclabs.cl/super-project/test-project/commit/e3426f9362ce904e2f20a8c6f91a3d3d9181eccc', u'timestamp': u'2015-09-02T15:10:47-03:00',
+                    u'message': u'Create readme\n',
+                    u'id': u'e3426f9362ce904e2f20a8c6f91a3d3d9181eccc',
+                    u'author': {
+                        u'name': u'Felipe Lalanne',
+                        u'email': u'flalanne@niclabs.cl'
+                        }
+                }
+            ],
+            u'after': u'e3426f9362ce904e2f20a8c6f91a3d3d9181eccc',
+            u'checkout_sha': u'e3426f9362ce904e2f20a8c6f91a3d3d9181eccc',
+            u'total_commits_count': 1,
+            u'message': "This is a message",
+            u'project_id': 83,
+            u'user_name': u'Felipe Lalanne',
+            u'user_email': u'flalanne@niclabs.cl',
+            u'before': u'0000000000000000000000000000000000000000'
+        }
+        rv = self.app.post('/hooks/gitlab', follow_redirects=True,
+                           data=json.dumps(data), content_type='application/json',
+                           headers={'X-Gitlab-Event': 'Tag Push Hook'})
+
+
+        assert rv.status_code == 200
+        assert rv.data == """Version 0.0.1 of <http://git.niclabs.cl/super-project/test-project|super-project/test-project> has been published by <@flalanne>. Let's celebrate team *super-project*!! :balloon::confetti_ball::tada:"""
+
     def test_issues_hook_without_namespace(self):
         data = {
           "object_kind": "issue",
@@ -35,7 +77,6 @@ class GitlabTestCase(BaseTestCase):
         rv = self.app.post('/hooks/gitlab', follow_redirects=True,
                            data=json.dumps(data), content_type='application/json',
                            headers={'X-Gitlab-Event': 'Issue Hook'})
-
 
         assert rv.status_code == 200
         assert rv.data == """Administrator opened <http://example.com/diaspora/issues/23|issue #23> in <http://example.com/diaspora|diaspora>: *New API: create/update/delete file*
