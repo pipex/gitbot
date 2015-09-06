67dab2c6b4e469dd88541293bfe0b9a5df51b8fb
Felipe Lalanne <flalanne@niclabs.cl>
2015-09-06 19:47:16 -0300

    Create Index object for to simplify index creation in redis

--- tests/redis.py
+++ tests/redis.py
@@ -7,9 +7,14 @@ from app import redis
 class Entity(redis.Model):
     __prefix__ = 'entity:'
 
+class NameIndex(redis.Index):
+    __prefix__ = 'name:'
+    __relation__ = Entity
+
 class RedisModelTestCase(BaseTestCase):
     def tearDown(self):
         Entity.deleteall()
+        NameIndex.deleteall()
 
     def test_model_operations(self):
         entity = Entity('one')
@@ -48,3 +53,49 @@ class RedisModelTestCase(BaseTestCase):
             assert False
         except AttributeError:
             assert True
+
+
+    def test_index_operations(self):
+        one = Entity('one')
+        one['name'] = 'First'
+        one['data'] = 'Number one'
+
+        two = Entity('two')
+        two['name'] = 'Second'
+        two['data'] = 'Number two'
+
+        NameIndex('First').set(one)
+        NameIndex('Second').set(two)
+
+        assert NameIndex('First').get().name == one.name
+        assert NameIndex('First').get().data == one.data
+
+        assert NameIndex('Second').get().name != one.name
+        assert NameIndex('Second').get().name == two.name
+        assert NameIndex('Second').get().data == two.data
+
+        first = False
+        second = False
+        for e in NameIndex.all():
+            if e.key == one.name:
+                first = True
+            if e.key == two.name:
+                second = True
+
+        assert first
+        assert second
+
+        first = False
+        second = False
+        for k, e in NameIndex.items():
+            if e.name == one.name:
+                first = True
+            if e.name == two.name:
+                second = True
+
+        assert first
+        assert second
+
+        NameIndex('First').delete()
+        assert not NameIndex('First').get()
+        assert not NameIndex.contains('First')
