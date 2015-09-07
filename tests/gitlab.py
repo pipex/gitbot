2df52b0a286f05b70c3b0e842702c57808c29e45
Felipe Lalanne <flalanne@niclabs.cl>
2015-09-07 11:51:29 -0300

    Fix spacing for pep8 linter in tests/gitlab.py

--- tests/gitlab.py
+++ tests/gitlab.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from flask import json
 from .base import BaseTestCase
 
+
 class GitlabTestCase(BaseTestCase):
     def test_tag_push_hook(self):
         data = {
@@ -27,7 +28,7 @@ class GitlabTestCase(BaseTestCase):
                     u'author': {
                         u'name': u'Felipe Lalanne',
                         u'email': u'flalanne@niclabs.cl'
-                        }
+                    }
                 }
             ],
             u'after': u'e3426f9362ce904e2f20a8c6f91a3d3d9181eccc',
@@ -43,35 +44,34 @@ class GitlabTestCase(BaseTestCase):
                            data=json.dumps(data), content_type='application/json',
                            headers={'X-Gitlab-Event': 'Tag Push Hook'})
 
-
         assert rv.status_code == 200
         assert rv.data.decode('utf-8') == """Version 0.0.1 of <http://git.niclabs.cl/super-project/test-project|super-project/test-project> has been published by <@flalanne>. Let's celebrate team *super-project*!! :balloon::confetti_ball::tada:"""
 
     def test_issues_hook_without_namespace(self):
         data = {
-          "object_kind": "issue",
-          "user": {
-            "name": "Administrator",
-            "username": "root",
-            "avatar_url": "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=40\u0026d=identicon"
-          },
-          "object_attributes": {
-            "id": 301,
-            "title": "New API: create/update/delete file",
-            "assignee_id": 51,
-            "author_id": 51,
-            "project_id": 14,
-            "created_at": "2013-12-03T17:15:43Z",
-            "updated_at": "2013-12-03T17:15:43Z",
-            "position": 0,
-            "branch_name": None,
-            "description": "Create new API for manipulations with repository",
-            "milestone_id": None,
-            "state": "opened",
-            "iid": 23,
-            "url": "http://example.com/diaspora/issues/23",
-            "action": "siopen"
-          }
+            "object_kind": "issue",
+            "user": {
+                "name": "Administrator",
+                "username": "root",
+                "avatar_url": "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=40\u0026d=identicon"
+            },
+            "object_attributes": {
+                "id": 301,
+                "title": "New API: create/update/delete file",
+                "assignee_id": 51,
+                "author_id": 51,
+                "project_id": 14,
+                "created_at": "2013-12-03T17:15:43Z",
+                "updated_at": "2013-12-03T17:15:43Z",
+                "position": 0,
+                "branch_name": None,
+                "description": "Create new API for manipulations with repository",
+                "milestone_id": None,
+                "state": "opened",
+                "iid": 23,
+                "url": "http://example.com/diaspora/issues/23",
+                "action": "siopen"
+            }
         }
 
         rv = self.app.post('/hooks/gitlab', follow_redirects=True,
