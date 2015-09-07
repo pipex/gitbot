57ca12aab0c28e347a00e5df4a079d46836d6b66
Felipe Lalanne <flalanne@niclabs.cl>
2015-09-07 16:46:09 -0300

    Remove beginning '_' from the result of camel_to_underscore

--- app/util.py
+++ app/util.py
@@ -10,7 +10,7 @@ under_pat = re.compile(r'_([a-z])')
 
 def camel_to_underscore(name):
     """Source: http://stackoverflow.com/a/17156414/4962672"""
-    return camel_pat.sub(lambda x: '_' + x.group(1).lower(), name)
+    return camel_pat.sub(lambda x: '_' + x.group(1).lower(), name).lstrip('_')
 
 def underscore_to_camel(name):
     """Source: http://stackoverflow.com/a/17156414/4962672"""
